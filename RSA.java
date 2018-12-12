563cbee3e64f90a001cbf31b29108684aa6cb21a
hohostriker888 <40499830+hohostriker888@users.noreply.github.com>
2018-12-12 17:05:20 +0300

    Add files via upload

---
+++ RSA.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class RSA {
+    public static void main(String args[]) throws IOException {
+
+        Abonent Bob = new Abonent();
+        Abonent Alice = new Abonent();
+        File file = new File("log.txt");
+        file.createNewFile();
+        FileWriter writer = new FileWriter(file);
+
+        int p = 3;
+        int q = 7;
+        writer.write("Выбраны p и q, равные: " + p + " и " + q + "\n\r");
+
+        int n = p * q;
+        writer.write("\nВычислено N: " + n + "\n\r");
+        int f = (p-1)*(q-1);
+        writer.write("\nВычислена функия Эйлера, равная: " + f + "\n\r");
+
+        int e = 7;
+        writer.write("\nПолучен открытый ключ e: " + e + "\n\r");
+        //int l = 0;
+        //int k = 3;
+        //System.out.println(k);
+        int d = closeKey(e, f);
+        writer.write("\nПолучен закрытый ключ d: " + d + "\n\r");
+        int m = 11;
+        Bob.M = m;
+        writer.write("\nВведено исходное сообщение: " + Bob.M + "\n\r");
+        Bob.C = (int) Math.pow(Bob.M, e) % n;
+        writer.write("\nБоб зашифровал исходное сообщение: " + Bob.C + "\n\r");
+        Alice.M = (int) Math.pow(Bob.C, d) % n;
+        writer.write("\nАлиса расшифровала сообщение: " + Alice.M + "\n\r");
+        writer.flush();
+        writer.close();
+
+    }
+
+    private static int closeKey(int e, int f){
+        int k = 1;
+        while ((float)((k * f + 1) % e) != 0 ) {
+            k++;
+        }
+        int d = (k * f + 1) / e;
+
+        return d;
+    }
+}
+
+
+class Abonent{
+    int M;
+    int C;
+}
+
+
